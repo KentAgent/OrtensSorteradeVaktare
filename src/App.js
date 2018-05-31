c127c3755cca0bdfe10c0ca4726f9e445a0d2670
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-05-31 14:19:06 +0200

    mer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import willesComponent from './WillesComponent/';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          <WillesComponent></WillesComponent>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
