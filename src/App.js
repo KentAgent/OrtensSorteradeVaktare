62548ab8afd68a40927719a632fa4cbaae34eeee
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-05-31 14:20:23 +0200

    ..mer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import willesComponent from './WillesComponent/';
+import WillesComponent from './WillesComponent/';
 
 class App extends Component {
   render() {
