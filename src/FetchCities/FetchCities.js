3c42dddda8b4b37e06d665aa4604a804deb18a89
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-06-06 15:31:53 +0200

    refresh btn

---
+++ src/FetchCities/FetchCities.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import './FetchCities.css'
+
+let citiesListPrinted = ''
+
+class FetchCities extends Component {
+
+    // Initial state when loading app
+    constructor(props) {
+        super(props)
+        this.state = {
+            citiesData: [],
+            citiesList: ''
+        }
+    }
+
+    // componentDidMount is called after the component has been rendered into the DOM
+    componentDidMount() {
+        fetch('http://cities.jonkri.se')
+        .then(response => {
+            return response.json()
+        }).then(result => {
+            console.log(result)
+            this.setState({
+                citiesData: result
+            })
+
+            let citiesArray = this.state.citiesData
+            for (let i = 0; i < citiesArray.length; i++) {
+                citiesListPrinted += citiesArray[i].name + ' ' + citiesArray[i].population + '\n'
+                console.log(citiesListPrinted)
+            }
+
+            this.setState({
+                citiesList: citiesListPrinted
+            })
+        }).catch(error => {
+            console.log('There was an error: ', error)
+        })
+    }
+
+    render() {
+        if (!this.state.citiesData) return <p>Loading...</p>
+        return (
+            <div className="FetchCities">
+                <p>{this.state.citiesList}</p>
+            </div>
+        )
+    }
+}
+
+export default FetchCities
--- src/WillesComponent/WillesComponent.js
+++ src/WillesComponent/WillesComponent.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Button from 'react-button-component';
 
 class willesComponent extends React.Component {
 
-  getImg(){
+  constructor(props){
+    super(props);
+    this.state = {
+      img_URL : ''
+    }
+  }
+  onNewImgClick = function(e){
+
+  }
+
+  componentDidMount(){
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response =>{
       return response.json();
@@ -15,29 +26,15 @@ class willesComponent extends React.Component {
     })
   }
 
-  constructor(props){
-    super(props);
-    this.state = {
-      img_URL : ''
-    }
-    this.getImg = this.getImg.bind(this)
-  }
-
-  componentDidMount(){
-    this.getImg();
-  }
-
   render(){
       return (
         <div>
           <h1>Tjenaaaa</h1>
-          <button onClick={this.getImg}>Refresh</button>
-
+          <Button onClick={() => console.log('dada')}></Button>
           <img src={this.state.img_URL} alt="Dog img"></img>
         </div>
     )
   }
-
 }
 
 export default willesComponent;
