bc62365600e7c85845309cc5a0f97c433bf84722
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-06-06 13:38:27 +0200

    willes komponent img fetch

--- src/WillesComponent/WillesComponent.js
+++ src/WillesComponent/WillesComponent.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const willesComponent = () => {
-  return (
-    <div>
-      <h1>Tjena</h1>
-    </div>
-  )
+class willesComponent extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      img_URL : ''
+    }
+
+  }
+
+  componentDidMount(){
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response =>{
+      return response.json();
+    })
+    .then(result =>{
+      console.log(result);
+      this.setState ({
+        img_URL : result.message
+      })
+    })
+  }
+
+  render(){
+      return (
+        <div>
+          <h1>Tjena</h1>
+          <img src={this.state.img_URL} alt="Dog img"></img>
+        </div>
+    )
+  }
 }
 
 export default willesComponent;
