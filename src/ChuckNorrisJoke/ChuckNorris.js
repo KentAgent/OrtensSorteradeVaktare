f5dd56e12aa531a2ce39e13a164a6ef7c781ebd0
ChristianP80 <christian.lars.persson@gmail.com>
2018-06-06 15:30:47 +0200

    moved code fot fetching data into function, added onclick on button to call this function

--- src/ChuckNorrisJoke/ChuckNorris.js
+++ src/ChuckNorrisJoke/ChuckNorris.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+
+
 class ChuckNorris extends React.Component {
 
   constructor(props) {
@@ -7,28 +9,35 @@ class ChuckNorris extends React.Component {
     this.state = {
       joke: ''
     }
+    this.fetchData = this.fetchData.bind(this)
   }
 
-componentDidMount() {
-  fetch('http://api.icndb.com/jokes/random')
-    .then(response => {
-      return response.json();
-     })
-    .then(result => {
-      console.log(result.value.joke)
-      this.setState({
-        joke: result.value.joke
-      })
-     });
+  fetchData() {
+    fetch('http://api.icndb.com/jokes/random')
+      .then(response => {
+        return response.json();
+       })
+      .then(result => {
+        console.log(result.value.joke)
+        this.setState({
+          joke: result.value.joke
+        })
+       });
+  }
+
+  componentDidMount() {
+    this.fetchData();
   }
 
 render() {
     return (
         <div className="FetchCities">
             <p>{this.state.joke}</p>
+            <button onClick={this.fetchData}>Refresh</button>
         </div>
     )
-}
+  }
+
 }
 
 
