40b6f1f75e8c67684c079966e93b1c7b45d20a02
danielmartinsson <e4g3f56f>
2018-06-06 13:36:51 +0200

    Joke API Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WillesComponent from './WillesComponent/WillesComponent.js';
-import FetchCities from './FetchCities/FetchCities'
+import FetchCities from './FetchCities/FetchCities';
+import JokeComponent from './JokeComponent/JokeComponent.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <WillesComponent />
-        <FetchCities /> 
+        <FetchCities />
+        <JokeComponent />
       </div>
     );
   }
--- src/JokeComponent/JokeComponent.js
+++ src/JokeComponent/JokeComponent.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react'
 
+// var jokeSetup;
+// var jokePunchline;
 
 class JokeComponent extends React.Component {
 
+  constructor(props) {
+      super(props)
+      this.state = {
+          jokeSetup: '',
+          jokePunchline: ''
+      }
+  }
+
+componentDidMount() {
+  fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
+  .then(response => {
+    return response.json();
+  })
+  .then(result => {
+    console.log(result)
+    this.setState({
+      jokeSetup: result.setup,
+      jokePunchline: result.punchline
+    })
+
+  })
+
 }
+render(){
+  return (
+    <div>
+      <h1>{this.state.jokeSetup}</h1>
+      <h1>{this.state.jokePunchline}</h1>
+    </div>
+  )
+}
+}
+
 
 export default JokeComponent
