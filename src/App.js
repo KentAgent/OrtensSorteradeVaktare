2f0ea954e2ac481d341505401558b25769ccc8dd
kakashidota <robin.kamo@live.se>
2018-06-06 12:15:35 +0200

    React Router setup. Grekolas + Kakashi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-import WillesComponent from './WillesComponent/WillesComponent.js';
-import FetchCities from './FetchCities/FetchCities'
+import WillesComponent from './Containers/HomePage/WillesComponent/WillesComponent.js';
+import FetchCities from './Containers/FetchCities/FetchCities'
+import HomePage from './Containers/HomePage/home'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <WillesComponent />
-        <FetchCities /> 
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/fetch-cities">Show cities</a></li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+        <HomePage/>
       </div>
+        </BrowserRouter>
     );
   }
 }
