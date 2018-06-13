80a65d9bbe22f2ce6913e8742d30383ec6d82721
William Lennartsson <william.j.lennartsson@gmail.com>
2018-06-13 14:11:25 +0200

    Reducer inits

--- src/actions/types.js
+++ src/actions/types.js
@@ -14,3 +14,5 @@ export const FETCH_TENJOKES = 'FETCH_TENJOKES'
 
 //Dog actions
 export const FETCH_DOG = 'FETCH_DOG'
+export const DOWNLOAD_DOG = 'DOWNLOAD_DOG'
+export const CLICK_THUMBNAIL = 'CLICK_THUMBNAIL'
