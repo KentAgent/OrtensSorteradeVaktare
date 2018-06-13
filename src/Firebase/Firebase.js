b42ae1de73edc1caa0b0c77067d905c391db5199
kakashidota <robin.kamo@live.se>
2018-06-13 13:42:48 +0200

    Get and delete up with firebase

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 var firebase = require('firebase')
 var config = {
@@ -24,6 +24,7 @@ class Firebase extends React.Component{
       this.getChat = this.getChat.bind(this);
       this.writeOnOurWall = this.writeOnOurWall.bind(this);
       this.getFirebaseList = this.getFirebaseList.bind(this);
+      this.deletePost = this.deletePost.bind(this);
   }
 
 getChat(){
@@ -45,18 +46,12 @@ getChat(){
 
 
   componentDidMount() {
-    this.readData();
-    this.populateTable();
+    this.update();
     //this.getChat();
     //this.getFirebaseList();
   }
 
-  readData(){
-    var mydb = firebase.database().ref('Wall');
-    mydb.on('value', function(snapshot){
-      //console.log(snapshot.val());
-    })
-}
+
 
    writeOnOurWall(){
       var name = this.state.value;
@@ -68,14 +63,12 @@ getChat(){
     });
   }
 
-  populateTable(){
 
-    var table = document.getElementById('mytable');
-    console.log(document.getElementById('mytable'));
-  //  var mylist = this.state.firebase.keys();
+
+  update(){
     var database = firebase.database();
     database.ref().on('value', function(element){
-      console.log(Object.entries(element.val().Wall)); // .map
+      console.log(Object.entries(element.val().Wall));
       var newList = Object.entries(element.val().Wall);
       this.setState({
         firebase: newList
@@ -85,8 +78,6 @@ getChat(){
 
 getFirebaseList(){
   var mylist = this.state.firebase.map(event =>{
-     console.log(event,'thihi')
-     console.log(event[1].message)
      var newList = this.state.firebase
      newList.push({key: event[0] ,message: event[1].message, username: event[1].username})
      console.log('newlist' + newList);
@@ -94,20 +85,24 @@ getFirebaseList(){
        firebase: newList
      })
  });
+ console.log(mylist);
 }
+
+deletePost(event, index){
+    var refe = firebase.database();
+    refe.ref('Wall/').child(index).remove();
+    console.log(refe.ref(index));
+
+}
+
    render(){
-
       const firebaseList = this.state.firebase.map(function(post){
-
             return <ul key={post[0]}>
               <li> {post[1].username}</li>
               <li> {post[1].message}</li>
+              <button onClick={(event)=>this.deletePost(event, post[0])}>Delete</button>
             </ul>
-      });
-
-
-
-
+      }.bind(this));
        return (
          <div>
            <h1>Tjena</h1>
@@ -118,9 +113,7 @@ getFirebaseList(){
            </div>
            <div>
              <input onChange={event => this.setState({value: event.target.value})} value={this.state.value} placeholder="Förnamn" type="text" />
-
              <input onChange={event => this.setState({message: event.target.value})} message={this.state.message} placeholder="Medelande" type="text" />
-
              <button onClick={this.writeOnOurWall}>Sign</button>
            </div>
          </div>
