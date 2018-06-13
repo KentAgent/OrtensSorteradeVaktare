a99385ca19a7c63c881b532cee3fc7a35ec60097
William Lennartsson <william.j.lennartsson@gmail.com>
2018-06-13 13:25:11 +0200

    Download link working

--- src/WillesComponent/WillesComponent.js
+++ src/WillesComponent/WillesComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './WillesComponent.css'
 import Lightbox from "./LightboxComponent"
 
+import FileSaver from 'file-saver'
+
 import Dog1 from './dog1.jpeg'
 import Dog2 from './dog2.jpg'
 import Dog3 from './dog3.jpg'
@@ -10,6 +12,18 @@ const download = require('image-downloader')
 
 
 class willesComponent extends React.Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+        img_URL : '',
+        imagesArray : [Dog1, Dog2, Dog3],
+        photoIndex: 0,
+      }
+      this.getImg = this.getImg.bind(this);
+      this.thumbNailClicked = this.thumbNailClicked.bind(this);
+      this.downloadImg = this.downloadImg.bind(this);
+  }
 
   getImg(){
     fetch('https://dog.ceo/api/breeds/image/random')
@@ -20,25 +34,12 @@ class willesComponent extends React.Component {
       console.log(result);
       var newImageArray = this.state.imagesArray;
       var img = result.message
-
       newImageArray.push(img);
       this.setState ({
         img_URL : result.message,
         imagesArray : newImageArray
+      })
     })
-  })
-}
-
-constructor(props){
-    super(props);
-    this.state = {
-      img_URL : '',
-      imagesArray : [Dog1, Dog2, Dog3],
-      photoIndex: 0,
-    }
-    this.getImg = this.getImg.bind(this);
-    this.thumbNailClicked = this.thumbNailClicked.bind(this);
-    this.downloadImg = this.downloadImg.bind(this);
   }
 
   componentDidMount(){
@@ -46,39 +47,27 @@ constructor(props){
   }
 
   downloadImg(){
-    if (this.state.img_URL === ''){
-      return;
-    }
-
-    var downloadOptions = {
-      url: this.state.img_URL,
-      dest: './saved_images/'
-    }
-
-    download.image(downloadOptions)
-    .then(({ filename, image }) => {
-      console.log('File saved to', filename)
+    var reader = new FileReader();
+    fetch('https://cors-anywhere.herokuapp.com/' + this.state.img_URL)
+    .then(function(response){
+      return response.blob();
     })
-    .catch((err) => {
-      console.error(err)
+    .then(function(blob){
+      FileSaver.saveAs(blob, 'doggle');
     })
-    }
+  }
 
   thumbNailClicked(e, index){
-
     this.setState({
       isOpen: true,
       photoIndex: index
     })
     {console.log(this.state.imagesArray, this.state.isOpen, this.state.photoIndex);}
-//    updateState(true, index)
     this.foo.updateState(true, index);
   }
 
-
   render(){
     const imgThumbNails = this.state.imagesArray.map((img, index) => {
-      // data-index={index}
         return <img src={img} className="thumbNail" key={index} onClick={(event) => this.thumbNailClicked(event, index)}/>
     })
       return (
@@ -88,7 +77,7 @@ constructor(props){
             </section>
           <div className="imgWrapper">
             <section><img className="imgBox" src={this.state.img_URL} alt="Dog img"></img></section>
-            <button className="refreshBtn" onClick={this.downloadImg}>Download</button>
+            <button className="refreshBtn" disabled={!this.state.img_URL} onClick={this.downloadImg}>Download</button>
             <div className ="thumbNailContainer">{imgThumbNails}</div>
           </div>
           <div><Lightbox images={this.state.imagesArray} photoIndex={this.state.photoIndex} ref={foo => {this.foo = foo;}} /></div>
