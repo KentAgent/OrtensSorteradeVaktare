64caff3342d6e717944484f1bad400b2e6d920b0
William Lennartsson <william.j.lennartsson@gmail.com>
2018-06-16 16:43:12 +0200

    Bug fixes

--- src/actions/dogActions.js
+++ src/actions/dogActions.js
@@ -1,11 +1,19 @@
 import {FETCH_DOG, DOWNLOAD_DOG, DOG_CLICK_THUMBNAIL} from './types'
 
 export const fetchDog = () => dispatch => {
-  console.log('hejhej dog');
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => {
+    return response.json();
+  })
+  .then(result => dispatch({
+    type: FETCH_DOG,
+    payload: result
+  })
+  )
 }
 
-export const downlodDog = () => dispatch => {
-
+export const downloadDog = () => dispatch => {
+  
 }
 
 export const dogThumbNailClicked = () => dispatch => {
--- src/reducers/dogReducer.js
+++ src/reducers/dogReducer.js
@@ -1,14 +1,30 @@
 import {FETCH_DOG, DOWNLOAD_DOG, DOG_CLICK_THUMBNAIL} from '../actions/types'
 
 const initialState = {
-
+  img_URL : '',
+  imagesArray : [],
+  photoIndex: 0
 }
 
 export default function(state = initialState, action) {
   switch (action.type) {
 
     case FETCH_DOG:
+      var newThumbnailArray = state.imagesArray;
+      newThumbnailArray.push(action.payload.message);
+      console.log('aa' , action.payload.message);
+      return {
+        ...state,
+        img_URL: action.payload.message,
+        imagesArray: newThumbnailArray
+      }
+
+    case DOWNLOAD_DOG:
       return {}
+
+    case DOG_CLICK_THUMBNAIL:
+      return {}
+
     default:
       return state
 
